0fa48e5a9a2f0a15331ad6d4fd9908a60cee5e88
louillustrator <lou.illustrator@gmail.com>
2018-09-04 16:58:47 +0100

    set up a homescreen button and initial map

---
+++ paint-the-town/components/Collection.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+class Collection extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default Collection;
