03bf0258ceb208f7fd47bc21f97edeca1e335ece
louillustrator <lou.illustrator@gmail.com>
2018-09-06 18:01:37 +0100

    gallery template done

--- paint-the-town/components/Collection.js
+++ paint-the-town/components/Collection.js
@@ -1,26 +1,50 @@
 import React, { Component } from "react";
-import { Text, View, Image, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  ScrollView,
+  Dimensions
+} from "react-native";
+const { width, height } = Dimensions.get("window");
 
 class Collection extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Image
-          style={styles.image}
-          source={require("../testImages/image.jpg")}
-        />
-        <Image
-          style={styles.image}
-          source={require("../testImages/image.jpg")}
-        />
-        <Image
-          style={styles.image}
-          source={require("../testImages/image.jpg")}
-        />
-        <Image
-          style={styles.image}
-          source={require("../testImages/image.jpg")}
-        />
+      <View style={styles.view}>
+        <Text style={styles.navBarText}>My Gallery</Text>
+
+        <ScrollView contentContainerStyle={styles.container}>
+          <Image
+            style={styles.image}
+            source={require("../testImages/image2.jpg")}
+          />
+          <Image
+            style={styles.image}
+            source={require("../testImages/image3.jpg")}
+          />
+          <Image
+            style={styles.image}
+            source={require("../testImages/image3.jpg")}
+          />
+          <Image
+            style={styles.image}
+            source={require("../testImages/image2.jpg")}
+          />
+          <Image
+            style={styles.image}
+            source={require("../testImages/image2.jpg")}
+          />
+          <Image
+            style={styles.image}
+            source={require("../testImages/image3.jpg")}
+          />
+          <Image
+            style={styles.image}
+            source={require("../testImages/image2.jpg")}
+          />
+        </ScrollView>
       </View>
     );
   }
@@ -28,18 +52,30 @@ class Collection extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     flexDirection: "row",
 
-    alignItems: "center",
-    justifyContent: "center",
-
-    backgroundColor: "#fa983a"
+    justifyContent: "flex-start",
+    flexWrap: "wrap",
+    backgroundColor: "#fff"
+  },
+  text: {
+    alignSelf: "center"
   },
   image: {
-    padding: 10,
-    width: 200,
-    height: 200
+    margin: 10,
+
+    width: 160,
+    height: 160
+  },
+
+  navBarText: {
+    backgroundColor: "#63cdda",
+    color: "white",
+    fontSize: 16,
+    fontWeight: "700",
+    textAlign: "center",
+    paddingTop: 20,
+    paddingBottom: 10
   }
 });
 
