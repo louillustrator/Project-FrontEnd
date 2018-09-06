80b48b888b4fe2d9975f79492e96da0119be7318
louillustrator <lou.illustrator@gmail.com>
2018-09-06 16:55:34 +0100

    homepage started and collections

--- paint-the-town/components/Collection.js
+++ paint-the-town/components/Collection.js
@@ -1,10 +1,46 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
 
 class Collection extends Component {
   render() {
-    return <div />;
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.image}
+          source={require("../testImages/image.jpg")}
+        />
+        <Image
+          style={styles.image}
+          source={require("../testImages/image.jpg")}
+        />
+        <Image
+          style={styles.image}
+          source={require("../testImages/image.jpg")}
+        />
+        <Image
+          style={styles.image}
+          source={require("../testImages/image.jpg")}
+        />
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+
+    alignItems: "center",
+    justifyContent: "center",
+
+    backgroundColor: "#fa983a"
+  },
+  image: {
+    padding: 10,
+    width: 200,
+    height: 200
+  }
+});
 
 export default Collection;
