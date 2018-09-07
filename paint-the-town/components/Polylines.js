66cca510f0b68a9bac81785c939494c52df40e27
Robert Davidson <r.a.davidson@live.co.uk>
2018-09-07 12:19:31 +0100

    add stop, start, pause, colour select and polylive functionality, refactored tracker to utils

--- paint-the-town/components/Polylines.js
+++ paint-the-town/components/Polylines.js
@@ -4,12 +4,13 @@ import MapView from 'react-native-maps'
 
 const Polylines = ({route}) => {
     return (<View>
-        {route.map((lineObj, index) => 
-        {return <MapView.Polyline
+        {route.map((path, index) =>
+        {
+          return <MapView.Polyline
           key={index}
-          coordinates={lineObj.line}
-          strokeWidth={lineObj.width}
-          strokeColor={lineObj.colour}
+          coordinates={path.latLng}
+          strokeWidth={path.width}
+          strokeColor={path.colour}
           lineCap="round"
           linejoin="round"
           />}
@@ -19,3 +20,20 @@ const Polylines = ({route}) => {
 };
 
 export default Polylines;
+
+// return (<View>
+//   {route.forEach(path => {path.latLng.map((coords, index) =>
+//   {
+//     let lineCoords = {latitude: coords.latitude, longitude: coords.longitude} 
+//     return <MapView.Polyline
+//     key={index}
+//     coordinates={lineCoords}
+//     strokeWidth={path.width}
+//     strokeColor={path.colour}
+//     lineCap="round"
+//     linejoin="round"
+//     />}
+//   )}
+// )}
+// </View>
+// );
--- paint-the-town/utils/Tracker.js
+++ paint-the-town/utils/Tracker.js
@@ -1,6 +1,7 @@
 import haversine from 'haversine';
+import { Location, Permissions, MapView } from "expo";
 
-export default _haversine = (coordArray) => {
+export const _haversine = (coordArray) => {
     if (coordArray.length > 1) {
         let metometer = 0;
         for (let i = 0; i < coordArray.length - 1; i++) {
@@ -11,3 +12,63 @@ export default _haversine = (coordArray) => {
     }
 }
 
+export const _watchPosition = function(val) {
+    if(val === 0 && this.state.watching === true) {
+        this.state.subscription.remove()
+    } else if(val === 1 && this.state.watching === false)
+    Permissions.askAsync(Permissions.LOCATION)
+      .then(res => {
+        if (res.status === "granted") {
+          returnedPromise = Location.watchPositionAsync(
+            {
+              enableHighAccuracy: true,
+              distanceInterval: 2,
+              timeInterval: 2000
+            },
+            newLocation => {
+              if (newLocation.timestamp) {
+                let object = {
+                  latitude: newLocation.coords.latitude,
+                  longitude: newLocation.coords.longitude,
+                  timestamp: newLocation.timestamp
+                };
+                let route = [...this.state.route]
+                let currentObj = {...route[route.length -1]}
+                let latLng = [...currentObj.latLng, object]
+                currentObj.latLng = latLng
+                route[route.length-1] = currentObj
+                let subscription = returnedPromise._55
+                this.setState({
+                  route,
+                  subscription
+                });
+              } else {
+                console.log("ignored newLocation");
+              }
+            }
+          );
+        }
+      })
+      .catch(err => console.log("Error in componentDidMount: ", err.message));
+  };
+
+  export const _getLocationAsync = async function() {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== "granted") {
+      this.setState({
+        errorMessage: "Permission to access location was denied"
+      });
+    }
+    let location = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true
+    });
+
+    this.setState({
+      status: "granted",
+      region: {
+        ...this.state.region,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude
+      }
+    });
+  };
