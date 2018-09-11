cff3092c32242bf04fb7fd36cbc468d9303f0e2d
Tara Galloway <taragalloway@rocketmail.com>
2018-09-11 14:41:40 +0100

    separated styles for reusability

---
+++ paint-the-town/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export default (exportStyles = StyleSheet.create({
+  text: {
+    fontFamily: "raleway-regular"
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "white",
+    opacity: 100,
+    padding: 17,
+    margin: 10,
+    width: "45%"
+  }
+}));
