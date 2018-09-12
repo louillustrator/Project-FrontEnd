50b249db8be0b03cd1ea3e197b9c4c6c3a73c3d1
louillustrator <lou.illustrator@gmail.com>
2018-09-12 15:45:43 +0100

    console log out

--- paint-the-town/components/Polylines.js
+++ paint-the-town/components/Polylines.js
@@ -1,79 +1,93 @@
-import React from 'react';
-import {Platform, View} from 'react-native';
-import MapView from 'react-native-maps'
+import React from "react";
+import { Platform, View } from "react-native";
+import MapView from "react-native-maps";
 
-const Polylines = ({route}) => {
-    if (Platform.OS === 'android'){
-      const iosRoute = getIosRoute(route)
-      return (<View>
-          {route.map((path, index) =>
-          {
-            return <MapView.Polyline
-            key={index}
-            coordinates={path.latLng}
-            strokeWidth={path.width}
-            strokeColor={path.colour}
-            lineCap="round"
-            linejoin="round"
-            />}
-          )}
-        </View>
-      );
-    } else {
-      const iosRoute = getIosRoute(route)
-      return (<View>
-        {iosRoute.coords.map((line, index) => {
-          return <MapView.Polyline
-          key={index}
-          coordinates={line}
-          strokeWidth={iosRoute.widthMap[index]}
-          strokeColors={iosRoute.colourMap[index]}
-          lineCap="round"
-          linejoin="round"
-          />
+const Polylines = ({ route }) => {
+  if (Platform.OS === "android") {
+    const iosRoute = getIosRoute(route);
+    return (
+      <View>
+        {route.map((path, index) => {
+          return (
+            <MapView.Polyline
+              key={index}
+              coordinates={path.latLng}
+              strokeWidth={path.width}
+              strokeColor={path.colour}
+              lineCap="round"
+              linejoin="round"
+            />
+          );
         })}
-      </View>)
-    }
+      </View>
+    );
+  } else {
+    const iosRoute = getIosRoute(route);
+    return (
+      <View>
+        {iosRoute.coords.map((line, index) => {
+          return (
+            <MapView.Polyline
+              key={index}
+              coordinates={line}
+              strokeWidth={iosRoute.widthMap[index]}
+              strokeColors={iosRoute.colourMap[index]}
+              lineCap="round"
+              linejoin="round"
+            />
+          );
+        })}
+      </View>
+    );
+  }
 };
 
-const getIosRoute = (route) => {
-  let coords = [], colourMap = [], widthMap = [];
+const getIosRoute = route => {
+  let coords = [],
+    colourMap = [],
+    widthMap = [];
   route.forEach((obj, index) => {
     if (index === 0) {
       coords.push(obj.latLng);
-      let currentObjColours = []
-      for (let i = 0; i < obj.latLng.length; i++){
-        currentObjColours.push(obj.colour)
+      let currentObjColours = [];
+      for (let i = 0; i < obj.latLng.length; i++) {
+        currentObjColours.push(obj.colour);
       }
-      colourMap.push(currentObjColours)
-      widthMap.push(obj.width)
-    } else if(obj.latLng[0] === route[index - 1].latLng[route[index - 1].latLng.length-1] && obj.width === route[index - 1].width){
-      coords[coords.length -1] = coords[coords.length -1].concat(obj.latLng)
-      let currentObjColours = []
-      for (let i = 0; i < obj.latLng.length; i++){
-        currentObjColours.push(obj.colour)
+      colourMap.push(currentObjColours);
+      widthMap.push(obj.width);
+    } else if (
+      obj.latLng[0] ===
+        route[index - 1].latLng[route[index - 1].latLng.length - 1] &&
+      obj.width === route[index - 1].width
+    ) {
+      coords[coords.length - 1] = coords[coords.length - 1].concat(obj.latLng);
+      let currentObjColours = [];
+      for (let i = 0; i < obj.latLng.length; i++) {
+        currentObjColours.push(obj.colour);
       }
-      colourMap[colourMap.length-1] = colourMap[colourMap.length-1].concat(currentObjColours)
+      colourMap[colourMap.length - 1] = colourMap[colourMap.length - 1].concat(
+        currentObjColours
+      );
     } else {
       coords.push(obj.latLng);
-      let currentObjColours = []
-      for (let i = 0; i < obj.latLng.length; i++){
-        currentObjColours.push(obj.colour)
+      let currentObjColours = [];
+      for (let i = 0; i < obj.latLng.length; i++) {
+        currentObjColours.push(obj.colour);
       }
-      colourMap.push(currentObjColours)
-      widthMap.push(obj.width)
+      colourMap.push(currentObjColours);
+      widthMap.push(obj.width);
     }
-  })
-  console.log(coords.length, colourMap.length, widthMap.length)
-  return {coords, colourMap, widthMap}
-}
+  });
+
+  return { coords, colourMap, widthMap };
+};
 
 export default Polylines;
 
 // return (<View>
 //   {route.forEach(path => {path.latLng.map((coords, index) =>
 //   {
-//     let lineCoords = {latitude: coords.latitude, longitude: coords.longitude} 
+//     let lineCoords = {latitude: coords.latitude, longitude: coords.longitude}
 //     return <MapView.Polyline
 //     key={index}
 //     coordinates={lineCoords}
